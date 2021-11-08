e4919b7b820ad0c1ddb5e59ac5aa31e94953b824
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-08 11:14:54 +0100

    Fix mobile pop up menu display

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelector(".ddl-menu").addEventListener("click", (event) => {
-    openMenu();
-  });
+  document.querySelectorAll(".ddl-menu, .popup-menu a, .close-menu").forEach((el)=>   {
+    el.addEventListener("click", (event) => {
+      openMenu();
+    });
+  })
+  
 
   function openMenu() {
-    const headerElement = document.querySelector("header");
-    headerElement.style.display === "block"
-      ? (headerElement.style.display = "block")
-      : (headerElement.style.display = "none");
     document.querySelector(".popup-menu").classList.toggle("open-menu");
   }
 });
