e96a0704d9f7c222aa215b094bc8e4509ee2d34f
ShadyShawkat <shadyshawkat91@gmail.com>
2021-11-08 11:34:50 +0200

    Add popup menu

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const str = 'asd';
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelector(".ddl-menu").addEventListener("click", (event) => {
+    openMenu();
+  });
+
+  function openMenu() {
+    const headerElement = document.querySelector("header");
+    headerElement.style.display === "block"
+      ? (headerElement.style.display = "block")
+      : (headerElement.style.display = "none");
+    document.querySelector(".popup-menu").classList.toggle("open-menu");
+  }
+});
