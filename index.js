cffe2496744b3ff69fa500843b1ce9e0f31e6297
ShadyShawkat <shadyshawkat91@gmail.com>
2021-11-08 13:46:32 +0200

    Fix linters' errors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelectorAll(".ddl-menu, .popup-menu a, .close-menu").forEach((el)=>   {
-    el.addEventListener("click", (event) => {
+function openMenu() {
+  document.querySelector('.popup-menu').classList.toggle('open-menu');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.ddl-menu, .popup-menu a, .close-menu').forEach((el) => {
+    el.addEventListener('click', () => {
       openMenu();
     });
-  })
-  
-
-  function openMenu() {
-    document.querySelector(".popup-menu").classList.toggle("open-menu");
-  }
+  });
 });
