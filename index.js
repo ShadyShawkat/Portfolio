7b8a18e722444387a524c2584678d710198dc8f6
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-09 10:33:35 +0100

    Add modal structure

--- index.js
+++ index.js
@@ -1,90 +1,88 @@
 function openMenu() {
-  document.querySelector(".popup-menu").classList.toggle("open-menu");
+  document.querySelector('.popup-menu').classList.toggle('open-menu');
 }
 
 const projects = [
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Data Dashboard Healthcare',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
   {
-    name: "Website Protfolio",
+    name: 'Website Protfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
   {
-    name: "Professional Art Printing Data",
+    name: 'Professional Art Printing Data',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
   {
-    name: "Website Protfolio",
+    name: 'Website Protfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
   {
-    name: "Professional Art Printing Data",
+    name: 'Professional Art Printing Data',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Data Dashboard Healthcare',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: "images/Img-placeholder.png",
-    technologies: ["html", "bootstrap", "Ruby"],
-    demoLink: "#",
-    srcLink: "#",
+    imageUrl: 'images/Img-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    demoLink: '#',
+    srcLink: '#',
   },
 ];
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   projects.forEach((project) => {
-    let div = document.createElement("div");
-    div.className = "project-card";
-    div.innerHTML = 
-        `<div class="project-card">
+    let div = document.createElement('div');
+    div.className = 'project-card';
+    div.innerHTML = `<div class="project-card">
           <div class="card-content">
             <h2 class="card-title">${project.name}</h2>
             <p class="txt-white">${project.description}</p>
             <ul class="card-technologies">
-              ${project.technologies.map((tech) => (
-                `<li>${tech}</li>`
-              )).join('')}
+              ${project.technologies.map((tech) => `<li>${tech}</li>`).join('')}
             </ul>
             </div>
             <button type="button" class="btn see-project-btn">
               See Project
             </button>
+            
           </div>`;
-    document.querySelector(".projects-cards").appendChild(div);
+    document.querySelector('.projects-cards').appendChild(div);
   });
 
   document
-    .querySelectorAll(".ddl-menu, .popup-menu a, .close-menu")
+    .querySelectorAll('.ddl-menu, .popup-menu a, .close-menu')
     .forEach((el) => {
-      el.addEventListener("click", () => {
+      el.addEventListener('click', () => {
         openMenu();
       });
     });
