1c7f8f0775ab8b7274aede8a1c65ce1b2a1dfd62
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-10 17:38:20 +0200

    Add todo project to the projects section

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ function persistFormData(element) {
 
 const projects = [
   {
-    name: 'Data Dashboard Healthcare',
+    id: 1,
+    name: 'TODO list',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: 'images/Img-placeholder.png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    demoLink: '#',
-    srcLink: '#',
+      "A website to keep all your daily todos. You can add, edit, complete and delete todos.",
+    imageUrl: 'images/todo-list-project-img.png',
+    technologies: ['html', 'css', 'javascript', 'webpacks'],
+    demoLink: 'https://shadyshawkat.github.io/TODO-List/dist/',
+    srcLink: 'https://github.com/ShadyShawkat/TODO-List',
   },
   {
+    id: 2,
     name: 'Website Protfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
@@ -30,6 +32,7 @@ const projects = [
     srcLink: '#',
   },
   {
+    id: 3,
     name: 'Professional Art Printing Data',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
@@ -39,6 +42,7 @@ const projects = [
     srcLink: '#',
   },
   {
+    id: 4,
     name: 'Website Protfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
@@ -48,6 +52,7 @@ const projects = [
     srcLink: '#',
   },
   {
+    id: 5,
     name: 'Professional Art Printing Data',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
@@ -57,6 +62,7 @@ const projects = [
     srcLink: '#',
   },
   {
+    id: 6,
     name: 'Data Dashboard Healthcare',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
@@ -95,7 +101,7 @@ window.addEventListener('DOMContentLoaded', () => {
   projects.forEach((project) => {
     const div = document.createElement('div');
     div.className = 'project-card';
-    div.innerHTML = `<div class="project-card">
+    div.innerHTML = `<div class="project-card" id="${project.id}">
           <div class="card-content">
             <h2 class="card-title">${project.name}</h2>
             <p class="txt-white">${project.description}</p>
@@ -106,8 +112,9 @@ window.addEventListener('DOMContentLoaded', () => {
             <button type="button" class="btn see-project-btn">
               See Project
             </button>
-            
           </div>`;
+          div.querySelector('.project-card').style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)),
+          url("${project.imageUrl}")`;
     document.querySelector('.projects-cards').appendChild(div);
   });
 
@@ -127,12 +134,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const close = document.querySelector('.close-modal');
 
   openButtons.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
+      const project = projects.find(project => project.id === parseInt(e.target.parentNode.id));
+      console.log(project)
       document.querySelector('body').style.overflow = 'hidden';
       document.querySelector('body').classList.add('blur');
       close.addEventListener('click', () => {
         modalContainer.classList.remove('active-modal');
       });
+      modalContainer.querySelector('.modal-title').textContent = project.name;
+      modalContainer.querySelector('.technologies-used-list').innerHTML = project.technologies.map(tech => `<li>${tech}</li>`).join("");
+      modalContainer.querySelector('.modal-img').src = project.imageUrl;
+      modalContainer.querySelector('.modal-text').textContent = project.description;
+      modalContainer.querySelector('.see-live-btn').href = project.demoLink;
+      modalContainer.querySelector('.see-src-btn').href = project.srcLink;
       modalContainer.classList.add('active-modal');
     });
   });
